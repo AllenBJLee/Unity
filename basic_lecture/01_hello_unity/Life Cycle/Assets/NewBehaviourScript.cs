1a2d79c4dbed29702cf68f6ee5e36a8434961f57
AllenLee <allenlee@AllenLeeui-MacBookPro.local>
2021-03-02 23:13:55 +0900

    Kaggle Exam
    Committed by AllenBJLee

---
+++ basic_lecture/01_hello_unity/Life Cycle/Assets/NewBehaviourScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    void Awake( )
+    {
+        Debug.Log( "Load Object Data" );
+    }
+
+    void OnEnable( )
+    {
+        Debug.Log( "Object Enabled" );
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log( "Hello, World" );
+    }
+
+    void FixedUpdate( )
+    {
+        Debug.Log( "Move" );
+    }
+
+    void Update( )
+    {
+        Debug.Log( "Update" );
+    }
+
+
+    void LateUpdate( )
+    {
+        Debug.Log( "Lazy Update" );
+    }
+
+    void OnDisable( )
+    {
+        Debug.Log( "Object Disabled" );
+    }
+
+    void OnDestroy( )
+    {
+        Debug.Log( "Unload Object Data");
+    }
+}
---
+++ basic_lecture/01_hello_unity/Move Object/Assets/MoveObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    Vector3 m_vecTarget = new Vector3( 5, 0.25f, 0 );
+
+    void Update( )
+    {
+        /// 01
+        /// 키 입력으로 이동
+        Vector3 vecMoveWithKeypad = new Vector3( Input.GetAxis("Horizontal"), 
+                                        Input.GetAxis("Vertical"),
+                                        0 );
+
+
+        /// 02
+        /// 목표 지점으로 이동
+        //transform.position = Vector3.MoveTowards( transform.position ,m_vecTarget, 1f );
+
+        /// 03
+        /// 목표 지점으로 스무스 하게 이동 (미끄러지든 감속 이동)
+        // Vector3 velo = Vector3.zero;
+        // transform.position = Vector3.SmoothDamp( transform.position, m_vecTarget, ref velo, 0.3f );
+
+        /// 04
+        /// Lerp (선형 보간 이동)
+        // transform.position = Vector3.Lerp( transform.position, m_vecTarget, 0.05f );
+
+        /// 05
+        /// Slerp (선형 포물선 보간 이동)
+        // transform.position = Vector3.Slerp( transform.position, m_vecTarget, 0.05f );
+    }
+}
